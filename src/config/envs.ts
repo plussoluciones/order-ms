0780013b561dbb2e2f7879054260cc46988389eb
Edgardo Alvarez <edgardo@livedesign.org>
2024-05-10 14:44:14 -0500

    commit init

---
+++ src/config/envs.ts
@@ -0,0 +1,28 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+
+interface EnvVars {
+  PORT: number;
+  // PRODUCTS_MICROSERVICE_HOST: string;
+  // PRODUCTS_MICROSERVICE_PORT: number;
+}
+
+const envsSchema = joi
+  .object({
+    PORT: joi.number().required(),
+  })
+  .unknown(true);
+
+const { error, value } = envsSchema.validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+const envVars: EnvVars = value;
+
+export const envs = {
+  port: envVars.PORT,
+  // productMicroserviceHost: envVars.PRODUCTS_MICROSERVICE_HOST,
+  // productMicroservicePort: envVars.PRODUCTS_MICROSERVICE_PORT,
+};
